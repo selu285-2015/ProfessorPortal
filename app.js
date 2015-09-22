66fd0955a9cdd2b5fabca197262068f7092f08c3
Patrick <patrick.dolan@selu.edu>
2015-09-21 23:30:49 -0500

    PoaerPoint!

--- app.js
+++ app.js
@@ -39,20 +39,6 @@ professorPortal.config(function($stateProvider, $urlRouterProvider) {
             controller: 'HomeController'
         });
         $urlRouterProvider.otherwise('/home');
-        // $routeProvider.
-        //     when('/PaoerPoint', {
-        //         templateUrl: 'Partials/_PaoerPoint.html',
-        //         controller  : 'PaoerPointController'
-        //     }).
-        //     when('/Test', {
-        //         templateUrl: "../Views/Partials/_Temp.html"
-        //     }).
-        //     otherwise({
-        //         redirect:'/'
-        //     });
-        // // use the HTML5 History API
-        // //$locationProvider.html5Mode(true);
-        // console.log($routeProvider);
     });
 
 function PaoerCtrl($scope) {
